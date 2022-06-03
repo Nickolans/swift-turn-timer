c4fb5b40b89820aa42c41b774513363bbcdef8ae
Nickolans Griffith <nickolansgriffith@gmail.com>
2022-06-03 16:18:49 -0500

    Initial Commit

---
+++ TurnTimer/TurnTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurnTimerApp.swift
+//  TurnTimer
+//
+//  Created by Nickolans Griffith on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TurnTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
